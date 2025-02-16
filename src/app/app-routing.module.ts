4d4c467690ed639bc7e04abb4a4caf2708b70977
walther <wfrchapp@gmail.com>
2025-02-16 18:39:49 -0500

    ajuste hash rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,22 @@ import { isAuthenticatedGuard, isNotAuthenticatedGuard } from './auth/guards';
 const routes: Routes = [
   {
     path: 'auth',
-    canActivate: [ isNotAuthenticatedGuard],
+    canActivate: [isNotAuthenticatedGuard],
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
   {
     path: 'dashboard',
-    canActivate: [ isAuthenticatedGuard],
+    canActivate: [isAuthenticatedGuard],
     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
   },
   {
-    path:'**',
-    redirectTo:'auth'
+    path: '**',
+    redirectTo: 'auth'
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
